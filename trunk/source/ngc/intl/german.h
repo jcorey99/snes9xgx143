b9b1bd44fceb34a1bd51d07dd52e6f97b7246004
dsbomb <dsbomb@5da43c5c-bd49-0410-ab3a-67b1559138c4>
2008-04-24 16:27:56 +0000

    separate language files, add german translation by nIxx

---
+++ trunk/source/ngc/intl/german.h
@@ -0,0 +1,90 @@
+/*
+ * ---------------------------------------------------------------------------
+ *  Deutsch/German by nIxx
+ * ---------------------------------------------------------------------------
+ */
+
+// Some general menu strings
+#define MENU_ON                 "AN"
+#define MENU_OFF                "AUS"
+#define MENU_EXIT               "Zurueck"
+#define MENU_PRESS_A            "Druecke A zum Fortsetzen"
+
+// Main menu
+#define MENU_MAIN_PLAY          "Spiel starten"
+#define MENU_MAIN_RESET         "Emulator zuruecksetzen"
+#define MENU_MAIN_LOAD          "Lade neues Spiel"
+#define MENU_MAIN_SAVE          "Speicherstand Manager"
+#define MENU_MAIN_INFO          "ROM Informationen"
+#define MENU_MAIN_JOYPADS       "Konfiguriere Joypads"
+#define MENU_MAIN_OPTIONS       "Emulator Optionen"
+#ifdef HW_RVL
+#define MENU_MAIN_RELOAD        "TP neuladen"
+#define MENU_MAIN_REBOOT        "Wii neustarten"
+#else
+#define MENU_MAIN_RELOAD        "PSO neuladen"
+#define MENU_MAIN_REBOOT        "Gamecube neustarten"
+#endif
+#define MENU_MAIN_CREDITS       "View Credits"
+
+// Media menu
+#define MENU_MEDIA_TITLE        "Lade ein Spiel"
+#define MENU_MEDIA_SDCARD       "Lade von SDCard"
+#define MENU_MEDIA_DVD          "Lade von DVD"
+#define MENU_MEDIA_STOPDVD      "Stoppe DVD Motor"
+#define MENU_MEDIA_STOPPING     "Stoppe DVD ... Warte"
+#define MENU_MEDIA_STOPPED      "DVD Motor gestoppt"
+
+// File menu
+#define MENU_FILE_TITLE         "Speicherstand Manager"
+#define MENU_FILE_MANAGER       "Manager"
+
+// Save menu
+#define MENU_SAVE_TITLE         "Manager"
+#define MENU_SAVE_SAVE          "Speichern"
+#define MENU_SAVE_LOAD          "Laden"
+#define MENU_SAVE_DEVICE        "Gerät"
+
+// Rom Information
+#define MENU_INFO_ROM           "ROM"
+#define MENU_INFO_ROMID         "ROMID"
+#define MENU_INFO_COMPANY       "Company"
+#define MENU_INFO_SIZE          "Size"
+#define MENU_INFO_SRAM          "SRAM"
+#define MENU_INFO_TYPE          "Type"
+#define MENU_INFO_CHECKSUM      "Checksum"
+#define MENU_INFO_TVTYPE        "TV Type"
+#define MENU_INFO_FRAMES        "Frames"
+#define MENU_INFO_CRC32         "CRC32"
+#define MENU_INFO_ENJOY         "Enjoy the past!"
+
+// Config Joypad menu
+#define MENU_CONFIG_TITLE       "Kontroller konfiguration"
+#define MENU_CONFIG_SETUP       "SETUP"
+#define MENU_CONFIG_A           "SNES Button A -"
+#define MENU_CONFIG_B           "SNES Button B -"
+#define MENU_CONFIG_X           "SNES Button X -"
+#define MENU_CONFIG_Y           "SNES Button Y -"
+#define MENU_CONFIG_ANALOG      "Analog Clip   -"
+#define MENU_CONFIG_UPDOWN      "Allow U+D / L+R"
+
+// Emulator Options menu
+#define MENU_EMU_TITLE          "Emulator Optionen"
+#define MENU_EMU_SOUND          "Sound Echo"
+#define MENU_EMU_STEREO         "Reverse Stereo"
+#define MENU_EMU_TRANSP         "Transparency"
+#define MENU_EMU_FPS            "FPS anzeigen"
+#define MENU_EMU_INTERP         "Interpolate"
+#define MENU_EMU_TIMER          "Timer"
+#define MENU_EMU_MULTITAP       "MultiTap"
+
+// Credits menu
+#define MENU_CREDITS_TITLE      "Snes9x 1.43 - GX Edition v0.1.2beta1"
+#define MENU_CREDITS_GCPORT     "GC Port"
+#define MENU_CREDITS_GXINFO     "GX Info"
+#define MENU_CREDITS_FONT       "Font"
+#define MENU_CREDITS_TEST       "Testing"
+#define MENU_CREDITS_GREETS     "Greets"
+#define MENU_CREDITS_EXTRAS     "Extras"
+#define MENU_CREDITS_SUPPORT    "Support"
+
---
+++ trunk/source/ngc/intl/spanish.h
@@ -0,0 +1,90 @@
+/*
+ * ---------------------------------------------------------------------------
+ *  Spanish by Lopst's Wiimote version
+ * ---------------------------------------------------------------------------
+ */
+
+// Some general menu strings
+#define MENU_ON                 "ON"
+#define MENU_OFF                "OFF"
+#define MENU_EXIT               "Volver atras"
+#define MENU_PRESS_A            "Pulsa A para Continuar"
+
+// Main menu
+#define MENU_MAIN_PLAY          "Jugar"
+#define MENU_MAIN_RESET         "Resetear Emulador"
+#define MENU_MAIN_LOAD          "Abrir Nuevo Juego"
+#define MENU_MAIN_SAVE          "Guardar/Abrir"
+#define MENU_MAIN_INFO          "Informacion ROM"
+#define MENU_MAIN_JOYPADS       "Config. Controles"
+#define MENU_MAIN_OPTIONS       "Opciones Emulador"
+#ifdef HW_RVL
+#define MENU_MAIN_RELOAD        "Reiniciar Cargador"
+#define MENU_MAIN_REBOOT        "Reiniciar Wii"
+#else
+#define MENU_MAIN_RELOAD        "PSO Reload"
+#define MENU_MAIN_REBOOT        "Reiniciar Gamecube"
+#endif
+#define MENU_MAIN_CREDITS       "Ver Creditos"
+
+// Media menu
+#define MENU_MEDIA_TITLE        "Abrir Un Juego"
+#define MENU_MEDIA_SDCARD       "Abrir desde SDCard"
+#define MENU_MEDIA_DVD          "Abrir desde DVD"
+#define MENU_MEDIA_STOPDVD      "Parar Motor DVD"
+#define MENU_MEDIA_STOPPING     "Parando DVD ... Espere"
+#define MENU_MEDIA_STOPPED      "Motor DVD Parado"
+
+// File menu
+#define MENU_FILE_TITLE         "Guardar Partidas"
+#define MENU_FILE_MANAGER       "Manager"
+
+// Save menu
+#define MENU_SAVE_TITLE         "Manager"
+#define MENU_SAVE_SAVE          "Guardar"
+#define MENU_SAVE_LOAD          "Abrir"
+#define MENU_SAVE_DEVICE        "Dispositivo"
+
+// Rom Information
+#define MENU_INFO_ROM           "ROM"
+#define MENU_INFO_ROMID         "ROMID"
+#define MENU_INFO_COMPANY       "Company"
+#define MENU_INFO_SIZE          "Size"
+#define MENU_INFO_SRAM          "SRAM"
+#define MENU_INFO_TYPE          "Type"
+#define MENU_INFO_CHECKSUM      "Checksum"
+#define MENU_INFO_TVTYPE        "TV Type"
+#define MENU_INFO_FRAMES        "Frames"
+#define MENU_INFO_CRC32         "CRC32"
+#define MENU_INFO_ENJOY         "Disfruta el passado!"
+
+// Config Joypad menu
+#define MENU_CONFIG_TITLE       "Configuracion del mando de GC"
+#define MENU_CONFIG_SETUP       "Configuracion "
+#define MENU_CONFIG_A           "Boton SNES A - "
+#define MENU_CONFIG_B           "Boton SNES B - "
+#define MENU_CONFIG_X           "Boton SNES X - "
+#define MENU_CONFIG_Y           "Boton SNES Y - "
+#define MENU_CONFIG_ANALOG      "Analog Clip   - "
+#define MENU_CONFIG_UPDOWN      "Allow U+D / L+R "
+
+// Emulator Options menu
+#define MENU_EMU_TITLE          "Opciones Emulador"
+#define MENU_EMU_SOUND          "Sounido Eco"
+#define MENU_EMU_STEREO         "Inverti Stereo"
+#define MENU_EMU_TRANSP         "Transparencias"
+#define MENU_EMU_FPS            "Mostrar FPS"
+#define MENU_EMU_INTERP         "Interpolar"
+#define MENU_EMU_TIMER          "Timer"
+#define MENU_EMU_MULTITAP       "MultiTap"
+
+// Credits menu
+#define MENU_CREDITS_TITLE      "Snes9x 1.43 - GX Edition v0.1.2beta1"
+#define MENU_CREDITS_GCPORT     "GC Port"
+#define MENU_CREDITS_GXINFO     "GX Info"
+#define MENU_CREDITS_FONT       "Font"
+#define MENU_CREDITS_TEST       "Testing"
+#define MENU_CREDITS_GREETS     "Greets"
+#define MENU_CREDITS_EXTRAS     "Extras"
+#define MENU_CREDITS_SUPPORT    "Support"
+
