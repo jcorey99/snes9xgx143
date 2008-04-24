19523489ab8d3f7b2cf9639e07e6fc4638c7974a
dsbomb <dsbomb@5da43c5c-bd49-0410-ab3a-67b1559138c4>
2008-04-24 22:41:59 +0000

    add Dutch translation by Adr990

---
+++ trunk/source/ngc/intl/dutch.h
@@ -0,0 +1,90 @@
+/*
+ * ---------------------------------------------------------------------------
+ *  English by dsbomb
+ * ---------------------------------------------------------------------------
+ */
+
+// Some general menu strings
+#define MENU_ON                 "AAN"
+#define MENU_OFF                "UIT"
+#define MENU_EXIT               "Terug naar vorige"
+#define MENU_PRESS_A            "Druk op A om door te gaan"
+
+// Main menu
+#define MENU_MAIN_PLAY          "Start Spel"
+#define MENU_MAIN_RESET         "Herstart Emulator"
+#define MENU_MAIN_LOAD          "Laad New Spel"
+#define MENU_MAIN_SAVE          "Save Manager"
+#define MENU_MAIN_INFO          "ROM Informatie"
+#define MENU_MAIN_JOYPADS       "Instellingen Controllers"
+#define MENU_MAIN_OPTIONS       "Emulator Opties"
+#ifdef HW_RVL
+#define MENU_MAIN_RELOAD        "TP Herladen"
+#define MENU_MAIN_REBOOT        "Herstart Wii"
+#else
+#define MENU_MAIN_RELOAD        "PSO Herlaad"
+#define MENU_MAIN_REBOOT        "Herlaad Gamecube"
+#endif
+#define MENU_MAIN_CREDITS       "Bekijk Credits"
+
+// Media menu
+#define MENU_MEDIA_TITLE        "Laad een Apel"
+#define MENU_MEDIA_SDCARD       "Laad van SDCard"
+#define MENU_MEDIA_DVD          "Laad van DVD"
+#define MENU_MEDIA_STOPDVD      "Stop DVD Motor"
+#define MENU_MEDIA_STOPPING     "Stop DVD ... Wacht"
+#define MENU_MEDIA_STOPPED      "DVD Motor Gestopt"
+
+// File menu
+#define MENU_FILE_TITLE         "Save Bestand Manager"
+#define MENU_FILE_MANAGER       "Manager"
+
+// Save menu
+#define MENU_SAVE_TITLE         "Manager"
+#define MENU_SAVE_SAVE          "Save"
+#define MENU_SAVE_LOAD          "Laad"
+#define MENU_SAVE_DEVICE        "Apperaat"
+
+// Rom Information
+#define MENU_INFO_ROM           "ROM"
+#define MENU_INFO_ROMID         "ROMID"
+#define MENU_INFO_COMPANY       "Company"
+#define MENU_INFO_SIZE          "Size"
+#define MENU_INFO_SRAM          "SRAM"
+#define MENU_INFO_TYPE          "Type"
+#define MENU_INFO_CHECKSUM      "Checksum"
+#define MENU_INFO_TVTYPE        "TV Type"
+#define MENU_INFO_FRAMES        "Frames"
+#define MENU_INFO_CRC32         "CRC32"
+#define MENU_INFO_ENJOY         "Geniet van het verleden!"
+
+// Config Joypad menu
+#define MENU_CONFIG_TITLE       "Gamecube Controller Instellingen"
+#define MENU_CONFIG_SETUP       "INSTELLINGEN"
+#define MENU_CONFIG_A           "SNES Knop A -"
+#define MENU_CONFIG_B           "SNES Knop B -"
+#define MENU_CONFIG_X           "SNES Knop X -"
+#define MENU_CONFIG_Y           "SNES Knop Y -"
+#define MENU_CONFIG_ANALOG      "Analog Clip   -"
+#define MENU_CONFIG_UPDOWN      "Statoe U+D / L+R"
+
+// Emulator Options menu
+#define MENU_EMU_TITLE          "Emulator Opties"
+#define MENU_EMU_SOUND          "Sound Echo"
+#define MENU_EMU_STEREO         "Reverse Stereo"
+#define MENU_EMU_TRANSP         "Transparency"
+#define MENU_EMU_FPS            "FPS Display"
+#define MENU_EMU_INTERP         "Interpolate"
+#define MENU_EMU_TIMER          "Timer"
+#define MENU_EMU_MULTITAP       "MultiTap"
+
+// Credits menu
+#define MENU_CREDITS_TITLE      "Snes9x 1.43 - GX Edition v0.1.2beta1"
+#define MENU_CREDITS_GCPORT     "GC Port"
+#define MENU_CREDITS_GXINFO     "GX Info"
+#define MENU_CREDITS_FONT       "Font"
+#define MENU_CREDITS_TEST       "Testing"
+#define MENU_CREDITS_GREETS     "Greets"
+#define MENU_CREDITS_EXTRAS     "Extras"
+#define MENU_CREDITS_SUPPORT    "Support"
+
