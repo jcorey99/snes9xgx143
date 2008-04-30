15de256714a04afe1805e22d4842cc80ea6bcd6e
dsbomb <dsbomb@5da43c5c-bd49-0410-ab3a-67b1559138c4>
2008-04-30 03:44:01 +0000

    Missed Dutch translation credit (Thanks Adr990!); keep the version line in intl.h instead of each other language file.

--- source/ngc/intl.h
+++ source/ngc/intl.h
@@ -3,6 +3,7 @@
  * Keep the max string length to 30 characters or bad things will happen
  */
 #define MENU_STRING_LENGTH      30
+#define MENU_SNES9X_TITLE      "Snes9x 1.43 - GX Edition v0.1.2beta1"
 
 #ifdef INTL_SPANISH
 #include "intl/spanish.h"
